cd1ecd1aecf3a1bc794c86db421488c790b0fe52
doublem-codes <m.milanesi004@studenti.unibs.it>
2019-05-15 17:14:11 +0200

    v1.0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
+import UniBSFpLib.src.it.unibs.fp.mylib.*;
 public class Main {
 
     public static void main(String[] args) {
-
+        it.unibs.fp.mylib.InputDati.leggiStringa("Insert expression: ");
     }
 }
